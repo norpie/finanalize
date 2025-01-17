3c1a5da054b7283ac144c2f4c91cd84a0e3d7beb
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-17 22:15:11 +0100

    feat(frontend): added shadcn-svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { Button } from '$lib/components/ui/button/index.js';
+    import * as Alert from '$lib/components/ui/alert/index.js';
+    import { toast } from 'svelte-sonner';
+
+    function handleClick() {
+        toast('Hello world!');
+    }
+</script>
+
+<Button onclick={handleClick}>Click me</Button>
+
+<Alert.Root>
+    <Alert.Title>Heads up!</Alert.Title>
+    <Alert.Description>You can add components to your app using the cli.</Alert.Description>
+</Alert.Root>
