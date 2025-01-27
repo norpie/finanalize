2adb52cf06544bd04b0c0b770ae5ce867357206f
simon <simon.verbandt@student.ehb.be>
2025-01-27 12:02:08 +0100

    feat: login and register page components

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { toast } from 'svelte-sonner';
+</script>
+
+<div class="flex h-screen w-full items-center justify-center px-4">
+	<Card.Root class="mx-auto max-w-sm">
+		<Card.Header>
+			<Card.Title class="text-2xl">Login</Card.Title>
+			<Card.Description>Enter your email below to login to your account</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<div class="grid gap-4">
+				<div class="grid gap-2">
+					<Label for="email">Email</Label>
+					<Input id="email" type="email" placeholder="me@example.com" required />
+				</div>
+				<div class="grid gap-2">
+					<div class="flex items-center">
+						<Label for="password">Password</Label>
+						<!-- <a href="##" class="ml-auto inline-block text-sm underline"> -->
+						<!--    Forgot your password? -->
+						<!-- </a> -->
+					</div>
+					<Input id="password" type="password" required />
+				</div>
+				<!-- <Button class="w-full" onclick={login}>Login</Button> -->
+				<!-- <Button variant="outline" class="w-full">Login with Google</Button> -->
+			</div>
+			<div class="mt-4 text-center text-sm">
+				Don't have an account?
+				<a href="##" class="underline" onclick={() => (current = 'register')}> Sign up </a>
+			</div>
+		</Card.Content>
+	</Card.Root>
+</div>
