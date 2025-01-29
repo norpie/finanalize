9b086db54847a7332bb8b0cb9355ec0c94505076
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-29 13:48:43 +0100

    feat: remove surrealize, not mature enough

--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -1,17 +1,41 @@
 use serde::{Deserialize, Serialize};
-use surrealdb::Uuid;
-use surrealize::Surrealize;
+use surrealdb::sql::Thing;
 
-#[derive(Debug, Clone, Serialize, Deserialize, Surrealize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
-    pub id: Uuid,
+    pub id: String,
     pub email: String,
     pub password: String,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ChangesetUser {
-    pub id: Option<Uuid>,
-    pub email: Option<String>,
-    pub password: Option<String>,
+pub struct FrontendUser {
+    pub id: String,
+    pub email: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SurrealDBUser {
+    pub id: Thing,
+    pub email: String,
+    pub password: String,
+}
+
+impl From<SurrealDBUser> for User {
+    fn from(user: SurrealDBUser) -> Self {
+        User {
+            id: user.id.id.to_string(),
+            email: user.email,
+            password: user.password,
+        }
+    }
+}
+
+impl From<SurrealDBUser> for FrontendUser {
+    fn from(user: SurrealDBUser) -> Self {
+        FrontendUser {
+            id: user.id.id.to_string(),
+            email: user.email,
+        }
+    }
 }
