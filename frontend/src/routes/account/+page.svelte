b225563fc86ac84ac1a571bb7d155f46206c71dc
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-31 20:44:50 +0100

    feat: use store user for account

--- frontend/src/routes/account/+page.svelte
+++ frontend/src/routes/account/+page.svelte
@@ -1,48 +1,33 @@
 <script lang="ts">
+    import Spinner from '$lib/components/spinner.svelte';
     import * as Card from '$lib/components/ui/card/index.js';
     import { Input } from '$lib/components/ui/input/index.js';
     import { Label } from '$lib/components/ui/label/index.js';
 
-    import Sidebar from '$lib/components/sidebar.svelte';
-
-    import type User from '../../models/user';
-    import { get } from '$lib/request';
-
-    import { onMount } from 'svelte';
-    import Spinner from '$lib/components/spinner.svelte';
-
-    let user: User | undefined = $state<User>();
-
-    onMount(async () => {
-        user = (await get<User>('v1/protected/me')).result;
-    });
+    import { user } from '$lib/store';
 </script>
 
-{#if user}
-    <Sidebar bind:user location="Account">
-        <div class="flex h-[100%] w-full items-center justify-center px-4">
-            <Card.Root class="mx-auto max-w-sm">
-                <Card.Header>
-                    <Card.Title class="text-2xl">Account</Card.Title>
-                    <Card.Description>This is you.</Card.Description>
-                </Card.Header>
-                <Card.Content>
-                    <div class="grid gap-4">
-                        <div class="grid gap-2">
-                            <Label for="email">Id</Label>
-                            <Input id="email" type="text" bind:value={user.id} required disabled />
-                        </div>
-                        <div class="grid gap-2">
-                            <Label for="email">Email</Label>
-                            <Input id="email" type="email" bind:value={user.email} required disabled />
-                        </div>
+<div class="flex h-[100%] w-full items-center justify-center px-4">
+    {#if $user}
+        <Card.Root class="mx-auto max-w-sm">
+            <Card.Header>
+                <Card.Title class="text-2xl">Account</Card.Title>
+                <Card.Description>This is you.</Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <div class="grid gap-4">
+                    <div class="grid gap-2">
+                        <Label for="email">Id</Label>
+                        <Input id="email" type="text" bind:value={$user.id} required disabled />
                     </div>
-                </Card.Content>
-            </Card.Root>
-        </div>
-    </Sidebar>
-{:else}
-    <div class="flex h-screen w-full items-center justify-center">
+                    <div class="grid gap-2">
+                        <Label for="email">Email</Label>
+                        <Input id="email" type="email" bind:value={$user.email} required disabled />
+                    </div>
+                </div>
+            </Card.Content>
+        </Card.Root>
+    {:else}
         <Spinner />
-    </div>
-{/if}
+    {/if}
+</div>
