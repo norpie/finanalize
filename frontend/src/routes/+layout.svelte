9f3b7b026b2e607653793917ce4912dff7e6f42b
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-31 20:01:56 +0100

    feat: move sidebar to layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,10 +4,40 @@
     import { ModeWatcher } from 'mode-watcher';
     import { Toaster } from '$lib/components/ui/sonner/index.js';
 
+    import Sidebar from '$lib/components/sidebar.svelte';
+    import Spinner from '$lib/components/spinner.svelte';
+
+    import { onMount } from 'svelte';
+
+    import type User from '../models/user';
+
+    import { get } from '$lib/request';
+
+    import { user } from '$lib/store';
+    import { page } from '$app/state';
+
+    const hideSidebarRoutes = ['/', '/login', '/register'];
+
     let { children } = $props();
+
+    onMount(async () => {
+        const newUser = (await get<User>('v1/protected/me')).result;
+        user.set(newUser);
+    });
 </script>
 
 <ModeWatcher />
 <Toaster />
-
-{@render children()}
+{#if user}
+    {#if !hideSidebarRoutes.includes(page.url.pathname)}
+        <Sidebar location="Dashboard">
+            {@render children()}
+        </Sidebar>
+    {:else}
+        {@render children()}
+    {/if}
+{:else}
+    <div class="flex h-screen w-full items-center justify-center">
+        <Spinner />
+    </div>
+{/if}
