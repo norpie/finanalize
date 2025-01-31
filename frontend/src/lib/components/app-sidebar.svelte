442ef8cae92858129a2616329f502e01403e7ce7
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-31 15:05:19 +0100

    feat: added all nav elements

--- frontend/src/lib/components/app-sidebar.svelte
+++ frontend/src/lib/components/app-sidebar.svelte
@@ -1,17 +1,50 @@
 <script lang="ts">
-    import NavMain from '$lib/components/nav-main.svelte';
     import NavProjects from '$lib/components/nav-projects.svelte';
-    import NavSecondary from '$lib/components/nav-secondary.svelte';
     import NavUser from '$lib/components/nav-user.svelte';
-    import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-    import TrendingUp from 'lucide-svelte/icons/trending-up';
-    import type { ComponentProps } from 'svelte';
 
-    let {
-        ref = $bindable(null),
-        user = $bindable(),
-        ...restProps
-    }: ComponentProps<typeof Sidebar.Root> = $props();
+    import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+
+    import TrendingUp from 'lucide-svelte/icons/trending-up';
+
+    import LayoutDashboard from 'lucide-svelte/icons/layout-dashboard';
+    import FileStack from 'lucide-svelte/icons/file-stack';
+
+    import Coins from 'lucide-svelte/icons/coins';
+    import User from 'lucide-svelte/icons/user';
+    import LogOut from 'lucide-svelte/icons/log-out';
+
+    let { ref = $bindable(null), user = $bindable(), ...restProps } = $props();
+
+    const pages = [
+        {
+            name: 'Dashboard',
+            location: '/dashboard',
+            icon: LayoutDashboard
+        },
+        {
+            name: 'Reports',
+            location: '/reports',
+            icon: FileStack
+        }
+    ];
+
+    const account = [
+        {
+            name: 'Account',
+            location: '/account',
+            icon: User
+        },
+        {
+            name: 'Credits',
+            location: '/credits',
+            icon: Coins
+        },
+        {
+            name: 'Logout',
+            location: '/logout',
+            icon: LogOut
+        }
+    ];
 </script>
 
 <Sidebar.Root bind:ref variant="inset" {...restProps}>
@@ -20,7 +53,7 @@
             <Sidebar.MenuItem>
                 <Sidebar.MenuButton size="lg">
                     {#snippet child({ props })}
-                        <a href="##" {...props}>
+                        <div {...props}>
                             <div
                                 class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"
                             >
@@ -29,18 +62,18 @@
                             <div class="grid flex-1 text-left text-sm leading-tight">
                                 <span class="truncate font-semibold">Finanalize</span>
                             </div>
-                        </a>
+                        </div>
                     {/snippet}
                 </Sidebar.MenuButton>
             </Sidebar.MenuItem>
         </Sidebar.Menu>
     </Sidebar.Header>
     <Sidebar.Content>
-        <!-- <NavMain items={data.navMain} /> -->
-        <!-- <NavProjects projects={data.projects} /> -->
-        <!-- <NavSecondary items={data.navSecondary} class="mt-auto" /> -->
+        <!-- <NavMain items={nav} /> -->
+        <NavProjects name={'Navigation'} items={pages} />
     </Sidebar.Content>
     <Sidebar.Footer>
+        <NavProjects name={'Account'} items={account} />
         <NavUser bind:user />
     </Sidebar.Footer>
 </Sidebar.Root>
