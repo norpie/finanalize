24d7eea7a8a2bc88557a56614c9861feeb5bedf5
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-31 22:21:35 +0100

    feat: added account ui for password change

--- frontend/src/routes/account/+page.svelte
+++ frontend/src/routes/account/+page.svelte
@@ -1,31 +1,48 @@
 <script lang="ts">
     import Spinner from '$lib/components/spinner.svelte';
+    import { Separator } from '$lib/components/ui/separator/index.js';
+    import Button from '$lib/components/ui/button/button.svelte';
     import * as Card from '$lib/components/ui/card/index.js';
     import { Input } from '$lib/components/ui/input/index.js';
     import { Label } from '$lib/components/ui/label/index.js';
 
     import { user } from '$lib/store';
+
+    let password1 = $state('');
+    let password2 = $state('');
+    let currentPassword = $state('');
 </script>
 
-<div class="flex h-[100%] w-full items-center justify-center px-4">
+<div class="flex h-[100%] w-full items-center justify-center p-12">
     {#if $user}
-        <Card.Root class="mx-auto max-w-sm">
-            <Card.Header>
-                <Card.Title class="text-2xl">Account</Card.Title>
-                <Card.Description>This is you.</Card.Description>
-            </Card.Header>
+        <Card.Root class="h-full w-full justify-between p-8">
             <Card.Content>
+                <Card.Title class="mb-4">Email</Card.Title>
                 <div class="grid gap-4">
                     <div class="grid gap-2">
-                        <Label for="email">Id</Label>
-                        <Input id="email" type="text" bind:value={$user.id} required disabled />
-                    </div>
-                    <div class="grid gap-2">
-                        <Label for="email">Email</Label>
                         <Input id="email" type="email" bind:value={$user.email} required disabled />
                     </div>
                 </div>
-            </Card.Content>
+                <Separator class="my-8" />
+                <Card.Title class="mb-4">Password</Card.Title>
+                <div class="grid gap-4">
+                    <div class="grid gap-2">
+                        <Label for="password">Password</Label>
+                        <Input id="password" type="password" bind:value={password1} required />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="confirm-password">Confirm Password</Label>
+                        <Input id="confirm-password" type="password" bind:value={password2} required />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="current-password">Current Password</Label>
+                        <Input id="current-password" type="password" bind:value={currentPassword} required />
+                    </div>
+                </div></Card.Content
+            >
+            <Card.Footer>
+                <Button type="submit">Save</Button>
+            </Card.Footer>
         </Card.Root>
     {:else}
         <Spinner />
