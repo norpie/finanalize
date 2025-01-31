40a96f86d988913143a4d2e109f51973362baf1a
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-01-31 20:46:56 +0100

    feat: updated credits and reports to use layout sidebar

--- frontend/src/routes/credits/+page.svelte
+++ frontend/src/routes/credits/+page.svelte
@@ -1,32 +1,11 @@
 <script lang="ts">
-    import Sidebar from '$lib/components/sidebar.svelte';
-
-    import type User from '../../models/user';
-    import { get } from '$lib/request';
-
-    import { onMount } from 'svelte';
-    import Spinner from '$lib/components/spinner.svelte';
-
-    let user: User | undefined = $state<User>();
-
-    onMount(async () => {
-        user = (await get<User>('v1/protected/me')).result;
-    });
 </script>
 
-{#if user}
-    <Sidebar bind:user location="Credits">
-        <div class="flex flex-1 flex-col gap-4 p-4">
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="aspect-video rounded-xl bg-muted/50"></div>
-                <div class="aspect-video rounded-xl bg-muted/50"></div>
-                <div class="aspect-video rounded-xl bg-muted/50"></div>
-            </div>
-            <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"></div>
-        </div>
-    </Sidebar>
-{:else}
-    <div class="flex h-screen w-full items-center justify-center">
-        <Spinner />
+<div class="flex flex-1 flex-col gap-4 p-4">
+    <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div class="aspect-video rounded-xl bg-muted/50"></div>
+        <div class="aspect-video rounded-xl bg-muted/50"></div>
+        <div class="aspect-video rounded-xl bg-muted/50"></div>
     </div>
-{/if}
+    <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"></div>
+</div>
