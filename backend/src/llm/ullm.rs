7834252257ae85eeffadc5f6de2971f517956fd3
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-05 14:25:57 +0100

    feat: added support for checking status, and check for model loaded/autoloading

--- backend/src/llm/ullm.rs
+++ backend/src/llm/ullm.rs
@@ -3,6 +3,7 @@ use crate::prelude::*;
 use super::LLMApi;
 
 use async_trait::async_trait;
+use once_cell::sync::Lazy;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -29,6 +30,23 @@ pub struct UllmCompletionResponse {
     completion: String,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UllmStatus {
+    status: String,
+    engine: Option<String>,
+    model: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum UllmStatusName {
+    #[serde(rename = "loaded")]
+    Loaded,
+    #[serde(rename = "unloaded")]
+    Unloaded,
+    #[serde(rename = "no_model")]
+    NoModel,
+}
+
 impl Default for UllmApi {
     fn default() -> Self {
         Self {
@@ -44,6 +62,13 @@ impl UllmApi {
         let response = self.client.get(&url).send().await?;
         let models = response.json::<Vec<UllmModel>>().await?;
         Ok(models)
+    }
+
+    pub async fn status(&self) -> Result<UllmStatus> {
+        let url = format!("{}/status", self.base_url);
+        let response = self.client.get(&url).send().await?;
+        let status = response.json::<UllmStatus>().await?;
+        Ok(status)
     }
 
     pub async fn unload(&self) -> Result<()> {
@@ -71,9 +96,18 @@ impl UllmApi {
     }
 }
 
+static DEFAULT_MODEL: Lazy<UllmModel> = Lazy::new(|| UllmModel {
+    engine: "EXLLAMAV2".into(),
+    name: "bartowski_Qwen2.5-Coder-32B-Instruct-exl2_4_25".into(),
+});
+
 #[async_trait]
 impl LLMApi for UllmApi {
     async fn generate(&self, prompt: String) -> Result<String> {
+        let status = self.status().await?;
+        if status.status == "unloaded" || status.status == "no_model" {
+            self.load(DEFAULT_MODEL.clone()).await?;
+        }
         let value: Value = self
             .client
             .post(format!("{}/complete", self.base_url))
@@ -99,10 +133,7 @@ mod tests {
         // api.unload().await.unwrap();
         let list = api.list().await.unwrap();
         dbg!(&list);
-        let mut model = list[3].clone();
-        dbg!(&model);
-        model.engine = "EXLLAMAV2".to_string();
-        // api.load(model).await.unwrap();
+        // api.load(DEFAULT_MODEL.clone()).await.unwrap();
         let prompt = "Question: How tall is the Brussels Madou tower?\nAnswer:".to_string();
         let generated = api.generate(prompt.clone()).await.unwrap();
         println!("generated: {}", generated);
