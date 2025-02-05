15c32aa759a2978332593d8bbf4868ac23840cc0
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-05 09:22:47 +0100

    fix: need send + sync for thread safe

--- backend/src/llm/mod.rs
+++ backend/src/llm/mod.rs
@@ -3,11 +3,7 @@ use crate::prelude::*;
 use async_trait::async_trait;
 
 #[async_trait]
-pub trait LLMApi {
-    async fn new() -> Result<Self>
-    where
-        Self: Sized;
-
+pub trait LLMApi: Send + Sync + 'static {
     /// Generate a response to a prompt, return the tokens as a string
     async fn generate(&self, prompt: String) -> Result<String>;
 }
