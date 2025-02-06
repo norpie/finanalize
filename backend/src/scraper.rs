b2af847385a45b151096827402237fe86ceb551a
SimonVerbandt <simon.verbandt@gmail.com>
2025-02-06 16:57:33 +0100

    feat: implement fully

--- backend/src/scraper.rs
+++ backend/src/scraper.rs
@@ -1,34 +1,45 @@
 use crate::prelude::*;
 use chromiumoxide::{Browser, BrowserConfig, Handler};
-use std::time::{Duration, Instant};
 use futures_util::StreamExt;
+use once_cell::sync::{Lazy, OnceCell};
+use std::sync::Arc;
+use std::time::{Duration, Instant};
 use tokio::spawn;
+use tokio::sync::{Mutex};
 use tokio::time::{sleep, timeout};
 
+#[derive(Debug, Clone)]
 pub struct BrowserWrapper {
-    browser: Browser,
-    handler: Handler,
+    browser: Arc<Browser>,
+    handler: Arc<Mutex<Handler>>,
 }
-pub async fn setup_browser() -> Result<BrowserWrapper>{
-    let browser_config = BrowserConfig::builder().with_head().build().unwrap();
+
+static INSTANCE: OnceCell<BrowserWrapper> = OnceCell::new();
+
+pub async fn setup_browser() -> Result<()> {
+    let browser_config = BrowserConfig::builder().no_sandbox().build().unwrap();
     let (browser, handler) = Browser::launch(browser_config).await?;
-    let wrapper = BrowserWrapper {browser, handler};
-    Ok(wrapper)
+    let wrapper = BrowserWrapper {
+        browser: Arc::new(browser),
+        handler: Arc::new(Mutex::new(handler)),
+    };
+    INSTANCE.set(wrapper).unwrap();
+    Ok(())
 }
-pub async fn scrape_page(
-    wrapper: BrowserWrapper,
-    url: String,
-) -> Result<String> {
-    let browser = wrapper.browser;
-    let mut handler = wrapper.handler;
+
+pub async fn scrape_page(url: String) -> Result<String> {
+    let wrapper = INSTANCE.get().unwrap();
+    let browser = wrapper.browser.clone();
+    let mut handler = wrapper.handler.clone();
     let handle = spawn(async move {
+        let mut locked_handler = handler.lock().await;
         loop {
-                if let Ok(Some(_event)) = timeout(Duration::from_secs(2), handler.next()).await {
-                    continue;
-                } else {
-                    dbg!("No event for 2 seconds, breaking...");
-                    break;
-                }
+            if let Ok(Some(_event)) = timeout(Duration::from_secs(2), locked_handler.next()).await {
+                continue;
+            } else {
+                dbg!("No event for 2 seconds, breaking...");
+                break;
+            }
         }
     });
     let page = browser.new_page(url).await?;
@@ -59,12 +70,10 @@ mod test {
 
     #[tokio::test]
     async fn test_scrape_page() {
-        let wrapper = setup_browser().await.unwrap();
+        setup_browser().await.unwrap();
+        sleep(Duration::from_secs(2)).await;
         let url = "https://example.com".to_string();
-        let result: String = scrape_page(wrapper, url)
-            .await
-            .unwrap()
-            .to_string();
+        let result: String = scrape_page(url).await.unwrap().to_string();
         println!("{}", result);
         assert!(result.contains("Example Domain"));
     }
