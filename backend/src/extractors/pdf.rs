0054885d5fd6c54749052212d8fc534142ffe571
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-07 15:11:47 +0100

    feat: use text extractor for pdf contents

--- backend/src/extractors/pdf.rs
+++ backend/src/extractors/pdf.rs
@@ -1,7 +1,10 @@
+use crate::prelude::*;
+use lopdf::Document;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
-use lopdf::{Document, Object};
-use crate::prelude::*;
+
+use super::text::TextExtractor;
+use super::ContentExtract;
 
 pub struct PDFExtractor;
 
@@ -16,8 +19,8 @@ impl PDFExtractor {
 
         let mut extracted_text = String::new();
 
-        for (_, (page_id, _)) in doc.get_pages().iter() {  
-            if let Ok(text) = doc.extract_text(&[*page_id]) {  
+        for (_, (page_id, _)) in doc.get_pages().iter() {
+            if let Ok(text) = doc.extract_text(&[*page_id]) {
                 extracted_text.push_str(&text);
                 extracted_text.push('\n');
             }
@@ -27,36 +30,6 @@ impl PDFExtractor {
             return Err(FinanalizeError::NotFound);
         }
 
-        Ok(Self::split_text(&extracted_text))
-    }
-
-    fn split_text(text: &str) -> Vec<String> {
-        let mut chunks = Vec::new();
-        let mut current_chunk = String::new();
-
-        for word in text.split_whitespace() {
-            if current_chunk.len() + word.len() >= 512 {
-                chunks.push(current_chunk.clone());
-                current_chunk.clear();
-            }
-            if !current_chunk.is_empty() {
-                current_chunk.push(' ');
-            }
-            current_chunk.push_str(word);
-        }
-
-        if !current_chunk.is_empty() {
-            chunks.push(current_chunk);
-        }
-
-        chunks
+        TextExtractor {}.extract(&extracted_text).await
     }
 }
-
-
-
-
-
-
-
-
