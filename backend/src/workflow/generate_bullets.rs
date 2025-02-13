3ce276fbfb883c1e18df5b2f6a8c025796655151
SimonVerbandt <simon.verbandt@gmail.com>
2025-02-13 17:52:38 +0100

    test: added testing for paragraph bullet generation

--- backend/src/workflow/generate_bullets.rs
+++ backend/src/workflow/generate_bullets.rs
@@ -17,7 +17,7 @@ pub struct GenerateBulletsJob;
 #[derive(Debug, Serialize)]
 struct GenerateBulletsInput {
     message: String,
-    title: String,
+    title: Title,
     headings: Vec<Heading>,
 }
 #[derive(Debug, Serialize, Deserialize)]
@@ -26,22 +26,26 @@ struct GenerateBulletsOutput {
 }
 
 // Prompt structs
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Title {
+    report_title: String,
+}
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Heading {
-    heading: String,
+    report_section_heading: String,
     description: String,
 }
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct Paragraph {
     heading: String,
-    has_bullets: Vec<String>,
+    paragraph: Vec<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct SurrealDBParagraph {
+struct SurrealDBParagraph {
     id: Thing,
     heading: String,
-    has_bullets: Vec<String>,
+    paragraph: Vec<String>,
 }
 
 #[async_trait]
@@ -55,47 +59,122 @@ impl Job for GenerateBulletsJob {
         _browser: BrowserWrapper,
     ) -> Result<()> {
         let prompt = prompting::get_prompt(db.clone(), "paragraph".into()).await?;
-        let mut db_message = db
-            .query("SELECT ->has_message->report_message FROM $report")
-            .bind(("report", report.clone()))
-            .await?;
-        let mut db_title = db
-            .query("SELECT ->has_title->report_title FROM $report")
-            .bind(("report", report.clone()))
+        let mut db_titles = db
+            .query("SELECT * FROM (SELECT ->has_title->title as titles FROM $report FETCH titles)[0].titles[0]")
+            .bind(("report", report.id.clone()))
             .await?;
         let mut db_section_headings = db
-            .query("SELECT ->has_section_heading->report_section_heading FROM $report")
-            .bind(("report", report.clone()))
+            .query("SELECT ->has_section_heading->section_headings as headings FROM $report FETCH headings")
+            .bind(("report", report.id.clone()))
             .await?;
 
-        let message: String = db_message
-            .take::<Option<String>>(0)?
-            .ok_or(FinanalizeError::NotFound)?;
-        let title: String = db_title
-            .take::<Option<String>>(0)?
-            .ok_or(FinanalizeError::NotFound)?;
+        let message: String = report.user_input.clone();
+        let titles: Vec<Title> = db_titles.take(0)?;
+        let title: &Title = titles.first().ok_or(FinanalizeError::NotFound)?;
         let headings: Vec<Heading> = db_section_headings
-            .take::<Option<Vec<Heading>>>(0)?
+            .take::<Option<Vec<Heading>>>("headings")?
             .ok_or(FinanalizeError::NotFound)?;
 
         let gen_bullets_task = Task::new(&prompt);
         let gen_bullets_input = GenerateBulletsInput {
             message,
-            title,
+            title: title.clone(),
             headings,
         };
         let gen_bullets_output: GenerateBulletsOutput =
             gen_bullets_task.run(llm, &gen_bullets_input).await?;
-
-        let sdb_paragraphs: SurrealDBParagraph = db
-            .create("paragraphs")
-            .content(gen_bullets_output.paragraphs)
-            .await?
-            .ok_or(FinanalizeError::UnableToGenerateBullets)?;
-        db.query("RELATE $report ->has_paragraph -> $paragraphs")
-            .bind(("report", report.clone()))
-            .bind(("paragraphs", sdb_paragraphs))
-            .await?;
+        let gen_paragraphs = gen_bullets_output.paragraphs;
+        for paragraph in gen_paragraphs.iter() {
+            let sdb_paragraph: SurrealDBParagraph = db
+                .create("paragraph")
+                .content(paragraph.clone())
+                .await?
+                .ok_or(FinanalizeError::UnableToGenerateBullets)?;
+            db.query("RELATE $report ->has_paragraph -> $paragraph")
+                .bind(("report", report.id.clone()))
+                .bind(("paragraph", sdb_paragraph.id.clone()))
+                .await?;
+        }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::llm::ollama::Ollama;
+    use crate::models::{ReportCreation, SurrealDBReport};
+    use crate::search::SearxNG;
+    use crate::{db, scraper};
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    struct TestSDBTitle {
+        id: Thing,
+        report_title: String,
+    }
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    struct TestTitle {
+        report_title: String,
+    }
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    struct TestSDBHeading {
+        id: Thing,
+        report_section_heading: String,
+        description: String,
+    }
+
+    #[tokio::test]
+    #[ignore = "Depends on external services"]
+    async fn test_generate_bullets() {
+        dotenvy::from_filename(".env").ok();
+        let db = db::connect().await.unwrap();
+        let llm = Arc::new(Ollama::default());
+        let search = Arc::new(SearxNG::new("http://localhost:8081"));
+        scraper::setup_browser().await.unwrap();
+        let browser = scraper::INSTANCE.get().unwrap().clone();
+        let creation = ReportCreation::new("Apple 2025 Q4 outlook".into());
+        let report: SurrealDBReport = db
+            .create("report")
+            .content(creation)
+            .await
+            .unwrap()
+            .unwrap();
+        dbg!(&report);
+        // Create the necessary input in the db
+        let title = TestTitle {
+            report_title: "Apple 2025 Q4 outlook".to_string(),
+        };
+        let sdb_title: TestSDBTitle = db.create("title").content(title).await.unwrap().unwrap();
+        db.query("RELATE $report ->has_title-> $title")
+            .bind(("report", report.id.clone()))
+            .bind(("title", sdb_title.id.clone()))
+            .await
+            .unwrap();
+        let headings = vec![
+            Heading {
+                report_section_heading: "Introduction".to_string(),
+                description: "A brief introduction to the report.".to_string(),
+            },
+            Heading {
+                report_section_heading: "iPhone".to_string(),
+                description: "Information about the new iPhone model.".to_string(),
+            },
+        ];
+        for heading in headings {
+            let sdb_heading: TestSDBHeading = db
+                .create("section_heading")
+                .content(heading)
+                .await
+                .unwrap()
+                .unwrap();
+            db.query("RELATE $report ->has_section_heading-> $heading")
+                .bind(("report", report.id.clone()))
+                .bind(("heading", sdb_heading.id.clone()))
+                .await
+                .unwrap();
+        }
+
+        let job = GenerateBulletsJob;
+        job.run(&report, db, llm, search, browser).await.unwrap();
     }
 }
