6ea781807c2da21227507e3b1d1bcaf45b1fe109
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-15 23:27:01 +0100

    feat: keep leading spaces in prompt script

--- db/generate_prompt_import.sh
+++ db/generate_prompt_import.sh
@@ -9,10 +9,11 @@ for PROMPT_ID in $PROMPT_IDS; do
     HBS_FILES=$(find $PROMPT_ID -type f -name "*.hbs")
     PROMPT_ID=$(basename $PROMPT_ID)
     HBS_FILE=$(echo $HBS_FILES | cut -d ' ' -f 1)
-    # replace `'` with `\'`
+    # Read file content, replace newlines with `\n`, and escape `'` with `\'`
     CONTENT=$(awk '{printf "%s\\n", $0}' $HBS_FILE | sed "s/'/\\\'/g")
+    IFS=''
     read -r -d '' STATEMENT <<EOM
-    UPSERT prompt:$PROMPT_ID SET template = '$CONTENT';
+UPSERT prompt:$PROMPT_ID SET template = '$CONTENT';
 EOM
-    echo $STATEMENT >> $GIT_ROOT/db/import_prompts.surql
+    echo $STATEMENT >>$GIT_ROOT/db/import_prompts.surql
 done
