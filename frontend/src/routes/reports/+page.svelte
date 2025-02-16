70288c7942aed758e6801a28c9cbbb711f2d8b3c
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-16 17:42:18 +0100

    feat: implement backend fetching for reports

--- frontend/src/routes/reports/+page.svelte
+++ frontend/src/routes/reports/+page.svelte
@@ -6,84 +6,30 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 	import ChevronLeft from 'lucide-svelte/icons/chevron-left';
 	import ChevronRight from 'lucide-svelte/icons/chevron-right';
-	import { get, post } from '$lib/request';
+	import { get, post, request } from '$lib/request';
 	import { onMount } from 'svelte';
 	import Spinner from '$lib/components/spinner.svelte';
 	import { toast } from 'svelte-sonner';
 	import { goto } from '$app/navigation';
 
-	const defaultSize = 8;
-
-	interface ReportPage {
-		page: number;
-		size: number;
-		total: number;
-		reports: Report[];
-	}
-
 	interface Report {
 		id: string;
-		title: string;
-		date: string;
+		user_input: string;
+		created_at: string;
 		status: string;
-		cost: number | null;
 	}
 
-	let currentPage: ReportPage | undefined = {
-		page: 1,
-		size: 8,
-		total: 6,
-		reports: [
-			{
-				id: 'laisknucrb09324',
-				title: 'Apple Inc. During COVID-19',
-				date: '2022-09-01',
-				status: 'Paid',
-				cost: 13241
-			},
-			{
-				id: 'psNH8c7rb9w03',
-				title: 'Effects of Territorial Disputes in the South China Sea on Semiconductor Industry',
-				date: '2022-09-10',
-				status: 'Payment Pending',
-				cost: 15350
-			},
-			{
-				id: 'nsa9pd8h796231',
-				title: 'Tesla Inc. Q3 2022 Earnings Report',
-				date: '2022-10-28',
-				status: 'Generating',
-				cost: null
-			},
-			{
-				id: 'nc32764bcaw',
-				title: 'The Future of Electric Vehicles and the companies leading the charge',
-				date: '2022-11-08',
-				status: 'Researching',
-				cost: null
-			},
-			{
-				id: 'n8902734cb0j',
-				title: 'Performance of banks during periods of economic uncertainty',
-				date: '2023-01-09',
-				status: 'Validating',
-				cost: null
-			},
-			{
-				id: '890c74n2309asdh',
-				title: "Credit Suisse's end and the UBS takeover",
-				date: '2024-07-19',
-				status: 'Pending',
-				cost: null
-			}
-		].toReversed()
-	};
+    let reports: Report[] = $state([]);
 
-	async function getPage(page: number) {
-		let size = currentPage?.size ?? defaultSize;
-		const query = `v1/reports/?page=${page}&size=${size}`;
-		currentPage = (await get<ReportPage>(query)).result;
-	}
+    function boop(page: number) {
+        console.log(page);
+    }
+
+	// async function getPage(page: number) {
+	// 	let size = currentPage?.size ?? defaultSize;
+	// 	const query = `v1/reports/?page=${page}&size=${size}`;
+	// 	currentPage = (await get<ReportPage>(query)).result;
+	// }
 
 	let newReportSubject: string = $state("")
     let dialogOpen = $state(false);
@@ -100,7 +46,7 @@
     }
 
 	onMount(async () => {
-		// await getPage(1);
+		reports = (await get<Report[]>("v1/protected/reports?page=0&perPage=20")).result;
 	});
 </script>
 
@@ -117,7 +63,7 @@
 		</Dialog.Content>
 	</Dialog.Root>
 	<div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-		{#if currentPage}
+		{#if reports}
 			<Table.Root>
 				<Table.Header>
 					<Table.Row>
@@ -128,11 +74,11 @@
 					</Table.Row>
 				</Table.Header>
 				<Table.Body>
-					{#each currentPage.reports as report, i (i)}
-						<Table.Row>
+					{#each reports as report, i (i)}
+						<Table.Row onclick={() => goto(`/reports/${report.id}`)}>
 							<Table.Cell class="font-medium">{report.status}</Table.Cell>
-							<Table.Cell>{report.date}</Table.Cell>
-							<Table.Cell>{report.title}</Table.Cell>
+							<Table.Cell>{new Date(report.created_at)}</Table.Cell>
+							<Table.Cell>{report.user_input}</Table.Cell>
 							<Table.Cell class="text-right"
 								>{#if report.cost}{report.cost} Credits{:else}Unavailable{/if}</Table.Cell
 							>
@@ -141,9 +87,9 @@
 				</Table.Body>
 			</Table.Root>
 			<Pagination.Root
-				count={currentPage.total}
-				perPage={currentPage.size}
-				onPageChange={(page) => getPage(page)}
+				count={reports.length}
+				perPage={reports.length}
+				onPageChange={(page) => boop(page)}
 			>
 				{#snippet children({ pages, currentPage })}
 					<Pagination.Content>
