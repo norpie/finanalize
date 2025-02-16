6662b1016afde9705bae2499e3da1af7265f1f2d
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-16 14:28:59 +0100

    feat: netowrk host backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -95,7 +95,7 @@ async fn main() -> Result<()> {
                     .service(get_reports),
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await?;
     Ok(())
