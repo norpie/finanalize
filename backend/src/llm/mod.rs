73d30afa85b94d89815cc2df2706be8cbc79f617
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-20 23:24:32 +0100

    feat: make ollama default static API

--- backend/src/llm/mod.rs
+++ backend/src/llm/mod.rs
@@ -1,9 +1,17 @@
+use std::sync::Arc;
+
 use crate::prelude::*;
 
 use async_trait::async_trait;
+use ollama::Ollama;
+use once_cell::sync::Lazy;
 
 pub mod ollama;
 pub mod ullm;
+
+pub static API: Lazy<Arc<dyn LLMApi>> = Lazy::new(|| {
+    Arc::new(Ollama::default())
+});
 
 #[async_trait]
 pub trait LLMApi: Send + Sync + 'static {
