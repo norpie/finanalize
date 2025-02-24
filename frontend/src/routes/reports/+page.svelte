dabdc925e6f20efa78202785a79eaca1c2e13454
Simon Verbandt <simon.verbandt@gmail.com>
2025-02-24 22:09:37 +0100

    refactor: move websocket frontend logic to correct [id] page

--- frontend/src/routes/reports/+page.svelte
+++ frontend/src/routes/reports/+page.svelte
@@ -21,36 +21,7 @@
 	}
 
 	let reports: Report[] = $state([]);
-	let sockets: Record<string, WebSocket> = {};
-	let shouldReconnect: boolean = true;
-	let reconnectTimeout: number = 2000;
 
-	function connectSocket(reportId: string){
-		if(!reportId || sockets[reportId]) return;
-		const url = import.meta.env.VITE_BACKEND_URL;
-		const ws = new WebSocket(`${url}/v1/protected/live/reports/${reportId}`);
-		ws.onmessage = (event) => {
-			const data = JSON.parse(event.data);
-			console.log(data);
-		}
-		ws.onerror = (event) => {
-			console.error(event);
-		}
-		ws.onclose = (event) => {
-			console.log(`Socket closed for ${reportId}: `, event);
-			delete sockets[reportId];
-			if(shouldReconnect){
-				setTimeout(() => connectSocket(reportId), reconnectTimeout);
-			}
-		}
-	}
-
-	function disconnectSockets(){
-		shouldReconnect = false;
-		Object.values(sockets).forEach((socket) => {
-			socket.close();
-		});
-	}
 	function boop(page: number) {
 		console.log(page);
 	}
@@ -110,15 +81,8 @@
 
 	onMount(async () => {
 		reports = (await get<Report[]>('v1/protected/reports?page=0&perPage=20')).result;
-		shouldReconnect = true;
-		reports.forEach((report) => {
-			connectSocket(report.id);
-		});
 	});
 
-	onDestroy(() => {
-	disconnectSockets();
-	});
 </script>
 
 <div class="flex flex-1 flex-col gap-4 p-4">
