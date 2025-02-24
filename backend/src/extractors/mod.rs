e3324bdbfdaf7d5f63c1d0573a5e8e1c8b9843bf
AbdelillahEl <104194817+AbdelillahEl@users.noreply.github.com>
2025-02-24 20:03:39 +0100

    fix: Updated mod

--- backend/src/extractors/mod.rs
+++ backend/src/extractors/mod.rs
@@ -2,25 +2,44 @@ use crate::prelude::*;
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 
-pub mod csv;
+// pub mod csv;
 pub mod html;
 pub mod text;
 pub mod md;
+// pub mod figure;
+pub mod pdf;
 
 #[async_trait]
 trait ContentExtract {
-    async fn extract(&self, file: &str) -> Result<Vec<Content>>;
+    async fn extract(&self, file_type: FileType) -> Result<Vec<Content>>;
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Content {
     Html(String),
     Text(String),
-    Csv(String),
+    // Figures(Vec<Figure>),
+    // Csv(String),
     MarkDown(String),
+    Pdf(Vec<u8>),
     
 }
+#[derive(Debug, Serialize, Deserialize)]
+pub enum FileType {
+    Html(String),
+    Text(String),
+    // Figures(Vec<Figure>),
+    // Csv(String),
+    MarkDown(String),
+    Pdf(Vec<u8>),
+}
 
+// #[derive(Debug, Serialize, Clone, Deserialize)]
+// pub struct Figure {
+//     pub url: String,
+//     pub alt_text: Option<String>,
+//     pub caption: Option<String>,
+// }
 
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct Data {
