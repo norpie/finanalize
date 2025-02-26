6405caeddc372a475749a1b48e924b7bb28f3ff6
SimonVerbandt <simon.verbandt@gmail.com>
2025-02-26 19:02:31 +0100

    chore: debug logs for pdf extractor

--- backend/src/extractors/pdf.rs
+++ backend/src/extractors/pdf.rs
@@ -3,6 +3,7 @@ use std::io::{BufWriter, Write};
 use super::{html::HTMLExtractor, Content, ContentExtract, FileType};
 use crate::prelude::*;
 use async_trait::async_trait;
+use log::debug;
 use lopdf::Document;
 use pdf_extract::HTMLOutput;
 // Import Error for `lopdf`
@@ -13,17 +14,19 @@ pub struct PdfExtractor;
 #[async_trait]
 impl ContentExtract for PdfExtractor {
     async fn extract(&self, file: FileType) -> Result<Vec<Content>> {
+        debug!("Extracting content from PDF...");
         let FileType::Pdf(buffer) = file else {
             return Err(FinanalizeError::ParseError(
                 "Invalid input type".to_string(),
             ));
         };
+        debug!("Valid PDF buffer received: {} bytes", buffer.len());
 
         // Perform PDF extraction in a blocking thread
         let html_content = task::spawn_blocking(move || {
             // Load the PDF document from the buffer using lopdf's `load_from` function
             let doc = Document::load_mem(&buffer)?;
-
+            debug!("PDF document loaded successfully");
             // Create a buffer to write into
             let mut buffer = Vec::new();
 
@@ -35,13 +38,13 @@ impl ContentExtract for PdfExtractor {
                 writer.flush()?;
             }
             let output = String::from_utf8(buffer)?;
-
+            debug!("Text extracted successfully");
             Ok::<_, FinanalizeError>(output) // Return the HTML-wrapped extracted text
         })
         .await??;
 
         let result = HTMLExtractor.extract(FileType::Html(html_content)).await?;
-
+        debug!("Content extracted successfully from PDF: {} chunks", result.len());
         // Return the extracted HTML content wrapped in Content::Html
         Ok(result)
     }
