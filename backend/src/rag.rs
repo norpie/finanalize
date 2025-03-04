ad776a955c91d03e6a7ea2b999ceef10c27055a7
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-03-04 12:36:45 +0100

    refactor: ragging is compatible with new FullReport workflow thing

--- backend/src/rag.rs
+++ backend/src/rag.rs
@@ -1,105 +1,40 @@
-use crate::{llm::LLMApi, prelude::*};
+use crate::{db::DB, llm::API, prelude::*};
 
 use log::debug;
 use serde::{Deserialize, Serialize};
-use std::sync::Arc;
 use surrealdb::sql::Thing;
 
-use crate::db::SurrealDb;
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Document {
-    pub id: String,
-    pub title: String,
-    pub description: String,
-    pub r#type: String,
-    pub url: String,
-}
-
-impl From<SurrealDbDocument> for Document {
-    fn from(doc: SurrealDbDocument) -> Self {
-        Document {
-            id: doc.id.id.to_string(),
-            title: doc.title,
-            description: doc.description,
-            url: doc.url,
-            r#type: doc.r#type,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct SurrealDbDocument {
-    pub id: Thing,
-    pub title: String,
-    pub description: String,
-    pub r#type: String,
-    pub url: String,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct DocumentChunk {
-    pub content: String,
+pub struct DistancedChunk {
+    pub report_id: String,
+    pub source_id: String,
+    pub chunk: String,
     pub distance: f64,
-    pub report_source: Document,
-}
-
-impl From<SurrealDbDocumentChunk> for DocumentChunk {
-    fn from(chunk: SurrealDbDocumentChunk) -> Self {
-        DocumentChunk {
-            content: chunk.content,
-            distance: chunk.distance,
-            report_source: chunk.report_source.into(),
-        }
-    }
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct SurrealDbDocumentChunk {
-    pub content: String,
-    pub distance: f64,
-    pub report_source: SurrealDbDocument,
 }
 
 static VECTOR_SEARCH_QUERY: &str = r#"
-SELECT content, distance, report_source FROM (SELECT
-    content,
-    vector::similarity::cosine(embedding, $search_embed) AS distance,
-    <-has_content_chunk<-report_source AS report_source
-FROM (
-    (SELECT
-        ->has_research->report_source->has_content_chunk->source_chunk AS chunks
-    FROM ONLY
-        $report
-    FETCH chunks)
-).chunks
-ORDER BY distance ASC)
-SPLIT report_source
-FETCH report_source;
+SELECT report_id, source_id, chunk, vector::similarity::cosine(embeddings, $embedding) AS distance FROM embedded_chunk WHERE report_id = $report_id;
 "#;
 
-pub async fn vector_search(
-    db: Arc<SurrealDb>,
-    llm_api: Arc<dyn LLMApi>,
-    report: Thing,
-    query: String,
-) -> Result<Vec<DocumentChunk>> {
+pub async fn vector_search(report: Thing, query: String) -> Result<Vec<DistancedChunk>> {
     debug!("Searching for '{:#?}'", &query);
-    let search_embed = llm_api.embed(query).await?;
+    let search_embed = API.clone().embed(query).await?;
     debug!("Embedding length: {}", &search_embed.len());
-    let results: Vec<SurrealDbDocumentChunk> = db
+    let results: Vec<DistancedChunk> = DB
+        .get()
+        .unwrap()
         .query(VECTOR_SEARCH_QUERY)
-        .bind(("search_embed", search_embed))
-        .bind(("report", report))
+        .bind(("embedding", search_embed))
+        .bind(("report_id", report.id.to_string()))
         .await?
         .take(0)?;
     debug!("Found {} results", results.len());
-    Ok(results.into_iter().map(DocumentChunk::from).collect())
+    Ok(results)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{db, llm::ollama::Ollama};
+    use crate::db;
 
     use super::*;
     use surrealdb::sql::Thing;
@@ -107,11 +42,10 @@ mod tests {
     #[tokio::test]
     #[ignore = "Depends on external service"]
     async fn test_vector_search() {
-        let db = Arc::new(db::connect().await.unwrap());
-        let llm_api = Arc::new(Ollama::default());
-        let report = Thing::from(("report", "jgq1yy5g4i5zfgv8w1xy"));
+        DB.set(db::connect().await.unwrap()).unwrap();
+        let report = Thing::from(("report", "sjaudnhcrlas"));
         let query = "Hello".to_string();
-        let results = vector_search(db, llm_api, report, query).await.unwrap();
+        let results = vector_search(report, query).await.unwrap();
         dbg!(&results);
         assert_eq!(results.len(), 3);
     }
