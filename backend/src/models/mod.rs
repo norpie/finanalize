6ce055275fc1b76dd74ee2d7e573343238975733
SimonVerbandt <simon.verbandt@gmail.com>
2025-03-05 13:11:13 +0100

    feat: added fields for charts and tables to report structs

--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -1,7 +1,6 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use surrealdb::sql::Thing;
-
 use crate::workflow::job::answer_questions::models::QuestionAnswer;
 use crate::workflow::job::classify_sources::models::ClassifiedSource;
 use crate::workflow::job::graphic_identifier::models::{Graphic, Text};
@@ -12,6 +11,7 @@ use crate::workflow::{
     },
     JobType,
 };
+use crate::workflow::job::generate_graphs::models::{GraphFileOutput, TableOutput};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
@@ -141,6 +141,8 @@ pub struct FullSDBReport {
     pub report: Option<String>,
     pub texts: Option<Vec<Text>>,
     pub graphics: Option<Vec<Graphic>>,
+    pub charts: Option<Vec<GraphFileOutput>>,
+    pub tables: Option<Vec<TableOutput>>
 }
 
 impl From<FullSDBReport> for FullReport {
@@ -168,6 +170,8 @@ impl From<FullSDBReport> for FullReport {
             report: report.report,
             texts: report.texts,
             graphics: report.graphics,
+            charts: report.charts,
+            tables: report.tables,
         }
     }
 }
@@ -202,6 +206,8 @@ pub struct FullReport {
     pub report: Option<String>,
     pub texts: Option<Vec<Text>>,
     pub graphics: Option<Vec<Graphic>>,
+    pub charts: Option<Vec<GraphFileOutput>>,
+    pub tables: Option<Vec<TableOutput>>
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -216,14 +222,18 @@ pub struct FrontendReport {
 #[cfg(test)]
 mod tests {
     use chrono::Utc;
-
     use super::{FullReport, PreClassificationSource};
     use crate::workflow::job::classify_sources::models::ClassifiedSource;
-    use crate::workflow::job::graphic_identifier::models::Text;
     use crate::workflow::{
         job::{chunk_content::models::Chunk, validation::models::ValidationOutput},
         JobType,
     };
+    use crate::workflow::job::graphic_identifier::models::{Graphic, Text};
+    use crate::workflow::{job::{
+        chunk_content::models::Chunk,
+        validation::models::ValidationOutput,
+    }, JobType};
+
 
     impl FullReport {
         pub fn new(id: String, user_input: String) -> Self {
@@ -254,6 +264,8 @@ mod tests {
                 report: None,
                 texts: None,
                 graphics: None,
+                charts: None,
+                tables: None
             }
         }
 
@@ -317,6 +329,11 @@ mod tests {
 
         pub fn with_chunks(mut self, chunks: Vec<Chunk>) -> Self {
             self.chunks = Some(chunks);
+            self
+        }
+        
+        pub fn with_graphics(mut self, graphics: Vec<Graphic>) -> Self {
+            self.graphics = Some(graphics);
             self
         }
     }
