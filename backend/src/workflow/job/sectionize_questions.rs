441d410bd2033dee8717923530cde0f61e64ca2a
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-03-09 16:37:51 +0100

    feat: implement sectionize_questions

---
+++ backend/src/workflow/job/sectionize_questions.rs
@@ -0,0 +1,53 @@
+use async_trait::async_trait;
+use log::debug;
+
+use crate::llm::API;
+use crate::tasks::Task;
+use crate::{prelude::*, prompting};
+
+use crate::workflow::WorkflowState;
+
+use super::Job;
+
+pub mod models {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct SectionizeQuestionsJobInput {
+        pub input: String,
+    }
+}
+
+pub struct SectionizeQuestionsJob;
+
+#[async_trait]
+impl Job for SectionizeQuestionsJob {
+    async fn run(&self, mut state: WorkflowState) -> Result<WorkflowState> {
+        let prompt = prompting::get_prompt("sectionize-questions".into())?;
+        let task = Task::new(&prompt);
+        let mut sections = Vec::new();
+        let sections_vec = state.state.question_answer_pairs.clone().unwrap();
+        let sections_len = sections_vec.len();
+        for (i, section_vec) in sections_vec.iter().enumerate() {
+            let sub_sections_len = section_vec.len();
+            let mut sub_sections = Vec::new();
+            for (j, sub_section_vec) in section_vec.iter().enumerate() {
+                let mut content = String::new();
+                for qa_pair in sub_section_vec {
+                    content.push('#');
+                    content.push_str(&qa_pair.question);
+                    content.push('\n');
+                    content.push('\n');
+                    content.push_str(&qa_pair.question);
+                    content.push('\n');
+                }
+                debug!("Running task for section {} of {} and sub-section {} of {}", i + 1, sections_len, j + 1, sub_sections_len);
+                let sub_section_content = task.run_raw(API.clone(), &content).await?;
+                sub_sections.push(sub_section_content);
+            }
+            sections.push(sub_sections);
+        }
+        state.state.sub_section_contents = Some(sections);
+        Ok(state)
+    }
+}
